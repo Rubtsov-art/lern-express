d254698963b6a9531446ae9f96907d6536f3b2ba
Pavel <pasharub60@gmail.com>
2020-08-25 08:45:22 +0500

    add dataBase

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
+const mongoose = require("mongoose")
 const coursesRoutes = require("./routes/courses")
 const cardRoutes = require("./routes/card")
 const addRoutes = require("./routes/add")
 const homeRouter = require("./routes/home")
+const path = require("path")
 
 const app = express()
 const hbs = exphbs.create({
@@ -15,8 +17,8 @@ app.engine("hbs", hbs.engine)
 app.set("view engine", "hbs")
 app.set("views", "views")
 
-app.use(express.static("public"))
-app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, "public")))
+app.use(express.urlencoded({ extended: true }))
 app.use("/courses", coursesRoutes)
 app.use("/add", addRoutes)
 app.use("/card", cardRoutes)
@@ -24,6 +26,19 @@ app.use("/", homeRouter)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+async function start() {
+    try {
+        const url = "mongodb+srv://Pavel:uNbkY0RBxHYG7pgD@cluster0.kfqbg.mongodb.net/shop"
+        await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
+
+
